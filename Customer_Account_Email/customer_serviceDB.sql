9ccb6285687826de9a070190ea74badbd0f1b143
Nguyen Hong Phu <nhoxpknguyen95@gmail.com>
2025-09-20 00:06:38 +0700

    Feat: Add customer_service account_service.py and their databases to Customer_Account_Email

---
+++ Customer_Account_Email/customer_serviceDB.sql
@@ -0,0 +1,34 @@
+﻿USE master;
+GO
+-- Đóng tất cả kết nối tới CustomerDB
+ALTER DATABASE CustomerDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+go
+
+IF EXISTS (SELECT name FROM sys.databases WHERE name = N'CustomerDB')
+BEGIN
+    DROP DATABASE CustomerDB;
+END
+
+CREATE DATABASE CustomerDB;
+GO
+
+USE CustomerDB;
+GO
+
+CREATE TABLE Customers (
+    customer_id NVARCHAR(50) PRIMARY KEY,
+    full_name NVARCHAR(100) NOT NULL,
+    phone_number NVARCHAR(15) NOT NULL,
+    email NVARCHAR(100) NOT NULL,
+	tuition_debt float default 0 
+);
+go	
+INSERT INTO Customers (customer_id, full_name, phone_number, email, tuition_debt)
+VALUES 
+('101', N'Nguyen Van A', '0909123456', 'a@example.com', 0),
+('102', N'Nguyen Van B', '0912345678', 'b@example.com', 1500000),
+('103', N'Nguyen Hong Phu', '0923456789', 'p@example.com', 500000),
+('104', N'Tran Thi C', '0934567890', 'c@example.com', 0),
+('105', N'Le Van D', '0945678901', 'd@example.com', 2500000);
+
+select * from Customers
