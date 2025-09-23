c8d1d217bbad735288a0c5f55dd4b25c9f470275
ThongChauPro-Coder <chauhuynhthong2022@gmail.com>
2025-09-23 10:19:17 +0700

    Remove payment_service folder

--- Backend_Final/payment_service/init_db.py
+++
@@ -1,9 +0,0 @@
-from .database import engine, Base
-from .models import Payment
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
-    print("Payment DB tables created")
-
-if __name__ == "__main__":
-    init_db()
--- Backend_Final/payment_service/main.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-from fastapi import FastAPI
-from .payment_router import router as payment_router
-
-app = FastAPI(title="Payment Service")
-app.include_router(payment_router)
-
-if __name__ == "__main__":
-    uvicorn.run("payment_service.main:app", host="0.0.0.0", port=8003, reload=True)
